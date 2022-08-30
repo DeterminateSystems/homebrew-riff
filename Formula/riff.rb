2e792c312123a9ef576eae25b28ba406e6f7e482
Luc Perkins <lucperkins@gmail.com>
2022-08-30 15:49:34 +0300

    Provide initial formula for Riff 1.0.0

--- Formula/riff.rb
+++ Formula/riff.rb
@@ -1,12 +1,18 @@
 class Riff < Formula
-  desc "A tool that automatically provides external dependencies1 for software projects"
+  desc "A tool that automatically provides external dependencies for software projects"
   homepage "https://github.com/DeterminateSystems/riff"
+  version "1.0.0"
   head "https://github.com/DeterminateSystems/riff.git", branch: "main"
-  url "https://github.com/DeterminateSystems/riff/archive/v0.1.0.tar.gz"
-  sha256 "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="
+  url "https://github.com/DeterminateSystems/riff/releases/download/v1.0.0/riff-x86_64-darwin"
+  sha256 "91f0ec51fc99f4a34240d54def73b2066d973fee48d8579bc775be7d63284bd1"
   license "MPL-2.0"
 
+  def install
+    bin.install "riff-x86_64-darwin" => "riff"
+  end
+
   test do
-    system "#{bin}/riff --help"
+    output = shell_output("#{bin}/riff --version").chomp
+    assert_match "riff 1.0.0", output
   end
 end
--- README.md
+++ README.md
@@ -3,3 +3,18 @@
 ```shell
 brew install DeterminateSystems/riff/riff
 ```
+
+> **Note**: This formula does *not* install [Nix] or [Cargo].
+
+## Updating the formula
+
+To update the `riff` formula for new versions of the tool:
+
+1. Update the `version` and `url` fields to reflect the new version.
+2. Run `brew install ./Formula/riff.rb`.
+3. Update the `sha256` value to match the value from the error output.
+4. Run `brew test ./Formula/riff.rb` to verify that the new version is properly
+  installable.
+
+[cargo]: https://doc.rust-lang.org/cargo
+[nix]: https://nixos.org
